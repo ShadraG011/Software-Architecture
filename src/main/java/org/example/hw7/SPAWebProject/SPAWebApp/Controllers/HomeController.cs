98a76d07c3a65776fd187ab79983a2cacf10e4f2
ShadraG011 <vgardash21@gmail.com>
2023-09-18 14:04:00 +1000

    add hw 7

---
+++ src/main/java/org/example/hw7/SPAWebProject/SPAWebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPAWebApplication.Models;
+
+namespace SPAWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Items()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
